e9e243ba125097a78054c1e91cb1cec0a3416a06
Bentot23 <92287648+Bentot23@users.noreply.github.com>
2023-02-08 13:54:53 -0600

    commit tree 9435c80a8ebd6fb82cfa3e460c36633526c15fe6

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,8 +1,18 @@
 class StudentsController < ApplicationController
 
   def index
+    # if params[:name]
+    #   students = Student.select{|student| student.first_name == params[:name] || student.last_name == params[:name]}
+    # else
+    #   students = Student.all
+    # end
     students = Student.all
     render json: students
+  end
+
+  def show
+    student = Student.find(params[:id])
+    render json: student
   end
 
 end
